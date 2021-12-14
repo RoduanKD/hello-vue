992dd5780fc552442d90b50204415841d5c133b5
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-12-15 00:01:39 +0200

    01 vue components  - button example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <v-button message="accept" activeColor="green" link="https://google.com"></v-button>
+    <v-button message="delete" activeColor="red"></v-button>
+    <v-button message="select" activeColor="blue"></v-button>
+    <v-button message="edit" activeColor="yellow"></v-button>
     <HelloWorld msg="Welcome to our Frontend Course"/>
+    <v-slider images="images" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VButton from './components/VButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    VButton
+  },
+  data () {
+    return {
+      images: []
+    }
   }
 }
 </script>
---
+++ src/components/VButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :style="style" @click="ghina">
+    {{ message }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: [
+    'message',
+    'activeColor',
+    'link'
+  ],
+  data () {
+    return {
+      count: 0,
+      style: {
+        backgroundColor: ''
+      }
+    }
+  },
+  methods: {
+    ghina () {
+      this.style.backgroundColor = this.activeColor
+      if (this.link) {
+        location.href = this.link
+      }
+    }
+  }
+}
+</script>
