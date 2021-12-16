a46a89f18ef92993abd15bea565829756cd45490
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-12-16 17:02:51 +0200

    events exercise

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <v-slider :images="images" />
+    <v-slider :images="images" @clear-images="clear" />
   </div>
 </template>
 
 <script>
+import VSlider from './components/VSlider.vue'
 // import HelloWorld from './components/HelloWorld.vue'
 // import VSlider here.
 
@@ -13,6 +14,7 @@ export default {
   name: 'App',
   components: {
     // import VSlider here Also.
+    VSlider
   },
   data () {
     return {
@@ -21,6 +23,12 @@ export default {
         'https://cdn.shopify.com/s/files/1/0017/0432/9285/files/programming-quotes-for-developers-made4dev-programmer-author.jpg?v=1539287556',
         'https://talgroupinc.files.wordpress.com/2017/04/programmers_april18.png?w=768'
       ]
+    }
+  },
+  methods: {
+    clear (fathi) {
+      console.log(fathi)
+      this.images = []
     }
   }
 }
--- src/components/VSlider.vue
+++ src/components/VSlider.vue
@@ -7,14 +7,22 @@
   </ul> -->
   <div class="slider">
     <button @click="previous">prev</button>
-    <img :src="selectedImage" alt="">
+    <img :src="selectedImage" alt="" width="800">
     <button @click="next">next</button>
+    <br>
+    <button @click="clear">clear</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['images'],
+  props: {
+    images: {
+      type: Array,
+      // required: true,
+      default: () => []
+    }
+  },
   data () {
     return {
       selectedIndex: 0
@@ -23,15 +31,23 @@ export default {
   computed: {
     selectedImage () {
       // return here the selected Image according to selected Index (remove null)
-      return null
+      return this.images[this.selectedIndex]
     }
   },
   methods: {
     previous () {
       // alter selected index to change selected image
+      this.selectedIndex--
+      if (this.selectedIndex === -1) { this.selectedIndex = this.images.length - 1 }
     },
     next () {
       // alter selected index to change selected image
+      this.selectedIndex++
+      if (this.selectedIndex === this.images.length) { this.selectedIndex = 0 }
+    },
+    clear () {
+      // this.images = []
+      this.$emit('clear-images', 'this is an argument')
     }
   }
 }
