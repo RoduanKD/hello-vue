c5adf71b5be9cb5394c053e6ab390be19bc22085
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-12-21 17:39:53 +0200

    change card style

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -6,35 +6,47 @@
           <v-card
             class="mx-auto"
             max-width="344"
-            outlined
           >
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="text-overline mb-4">
-                  OVERLINE
-                </div>
-                <v-list-item-title class="text-h5 mb-1">
-                  Headline 5
-                </v-list-item-title>
-                <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-              </v-list-item-content>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
 
-              <v-list-item-avatar
-                tile
-                size="80"
-                color="grey"
-              ></v-list-item-avatar>
-            </v-list-item>
+            <v-card-title>
+              Top western road trips
+            </v-card-title>
+
+            <v-card-subtitle>
+              1,000 miles of wonder
+            </v-card-subtitle>
 
             <v-card-actions>
               <v-btn
-                outlined
-                rounded
+                color="orange lighten-2"
                 text
               >
-                Button
+                Explore
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn
+                icon
+                @click="show = !show"
+              >
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
               </v-btn>
             </v-card-actions>
+
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                </v-card-text>
+              </div>
+            </v-expand-transition>
           </v-card>
         </v-col>
       </v-row>
@@ -44,7 +56,11 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      show: false
+    }
+  }
 }
 </script>
 
